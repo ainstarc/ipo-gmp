043fbe7a6f4867b52fc5d2e77dc7c1a68858d5af
Ain <122215301+ainstarc@users.noreply.github.com>
2025-06-28 15:27:57 +0530

    fix: next export

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/ipo-gmp" : "",
-  images: { unoptimized: true },
-  trailingSlash : true,
-};
+const nextConfig = {
+    output: 'export',
+      images: {
+          unoptimized: true,
+            },
+              trailingSlash: true,
+                  basePath: '/ipo-gmp',
+                    assetPrefix: '/ipo-gmp/',
+                    };
 
-export default nextConfig;
+                    export default nextConfig;
